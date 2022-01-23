2fa6f0549cb77673c1fe2c7d307bd92ce9cc78a7
Thebigbot <77632836+Thebigbot0000@users.noreply.github.com>
2022-01-23 09:23:49 +0100

    add emoji lang

--- server.js
+++ server.js
@@ -7,44 +7,58 @@ const atbash = require("./json/atbash.json")
 const kenny = require("./json/kenny.json")
 const morse = require("./json/morse.json")
 const rot = require("./json/rot.json")
+const emoji = require("./json/emoji.json")
 const fetch = require('node-fetch');
 const { letterTrans } = require(`custom-translate`)
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.write();
+    res.write("<center><h1>CyphererAPI</h1></center>");
     res.end();
 });
 
 app.post('/api/crypt', (req, res) => {
     if (!req.body.type || !req.body.type == ['atbash', 'morse', 'rot', 'kenny']) return res.json({ status: 'err' })
+    let text = req.body.text.toLowerCase()
     try {
         if (req.body.type == 'atbash') {
             res.json({
                 status: 'success',
-                text: letterTrans(req.body.text, atbash)
+                type: req.body.type,
+                text: letterTrans(text, atbash)
             })
         }
 
         if (req.body.type == 'morse') {
             res.json({
                 status: 'success',
-                text: letterTrans(req.body.text, morse)
+                type: req.body.type,
+                text: letterTrans(text, morse)
             })
         }
 
         if (req.body.type == 'rot') {
             res.json({
                 status: 'success',
-                text: letterTrans(req.body.text, rot)
+                type: req.body.type,
+                text: letterTrans(text, rot)
             })
         }
 
         if (req.body.type == 'kenny') {
             res.json({
                 status: 'success',
-                text: letterTrans(req.body.text, kenny)
+                type: req.body.type,
+                text: letterTrans(text, kenny)
+            })
+        }
+
+        if (req.body.type == 'emoji') {
+            res.json({
+                status: 'success',
+                type: req.body.type,
+                text: letterTrans(text, emoji)
             })
         }
     } catch {
@@ -59,7 +73,7 @@ http.listen(port, function () {
   headers: {
     'Content-Type': 'application/json',
   },
-body: JSON.stringify({ type: 'morse', text: "Ciao Come va?" })
+body: JSON.stringify({ type: 'emoji', text: "Ciao Come va?" })
 })
 .then(response => response.json())
 .then(data => console.log(data));
